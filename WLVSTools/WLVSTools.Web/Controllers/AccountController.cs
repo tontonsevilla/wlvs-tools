a659efec1f08a2bb72ec457bb09a4f306c08d54b
Tonton Sevilla <winston_sevilla@outlook.com>
2023-12-24 16:21:59 +0800

    Registration

---
+++ WLVSTools/WLVSTools.Web/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WLVSTools.Web.Infrastructure.Authentication;
+using WLVSTools.Web.ViewModels.Accounts;
+
+namespace WLVSTools.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(ApplicationUser applicationUser)
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(viewModel.Email);
+
+                if (user == null)
+                {
+                    var resultCreateAsync = await _userManager.CreateAsync(new ApplicationUser
+                    {
+                        Email = viewModel.Email,
+                        UserName = viewModel.Email,
+                        FirstName = viewModel.FirstName,
+                        LastName = viewModel.LastName
+                    }, viewModel.Password);
+
+                    if (resultCreateAsync.Succeeded)
+                    {
+                        return RedirectToAction("RegistrationSuccessful");
+                    }
+
+                    ModelState.AddModelError("", string.Join(", ", resultCreateAsync.Errors.Select(ie => ie.Description)));
+                }
+
+                ModelState.AddModelError("", "Already exists.");
+            }
+
+            return View(viewModel);
+        }
+
+        public IActionResult RegistrationSuccessful()
+        {
+            return View();
+        }
+    }
+}
---
+++ WLVSTools/WLVSTools.Web/ViewModels/Accounts/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WLVSTools.Web.ViewModels.Accounts
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+        [Required]
+        public string? ConfirmPassword { get; set; }
+
+    }
+}
