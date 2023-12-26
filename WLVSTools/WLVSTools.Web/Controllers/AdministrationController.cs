1a7de3cc1e6fe502027765bd83e54b6ae742ad76
Tonton Sevilla <winston_sevilla@outlook.com>
2023-12-26 13:57:09 +0800

    Administrattion - Databases

---
+++ WLVSTools/WLVSTools.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WLVSTools.Web.Controllers
+{
+    [Authorize]
+    public class AdministrationController : Controller
+    {
+        public IActionResult Databases()
+        {
+            return View(Directory.EnumerateFiles("Database", "*.*", SearchOption.TopDirectoryOnly)
+                .Where(s => s.EndsWith(".db")).ToList());
+        }
+    }
+}
