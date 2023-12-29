d24a913036d7683e4d3b30cae881b139529b4bd6
Tonton Sevilla <winston_sevilla@outlook.com>
2023-12-29 08:55:47 +0800

    Personal Toolbox - Lotto WIP

--- WLVSTools/WLVSTools.Web/Controllers/PersonalToolboxController.cs
+++ WLVSTools/WLVSTools.Web/Controllers/PersonalToolboxController.cs
@@ -4,8 +4,11 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using WLVSTools.Web.Core.Data.PersonalToolsEntities;
-using WLVSTools.Web.Core.Models;
+using WLVSTools.Web.Core.General;
 using WLVSTools.Web.Infrastructure.Authentication;
 using WLVSTools.Web.Infrastructure.PersonalTools;
 using WLVSTools.Web.ViewModels.PersonalToolbox;
@@ -15,6 +18,7 @@ namespace WLVSTools.Web.Controllers
     [Authorize]
     public class PersonalToolboxController : BaseController
     {
+        private readonly IWebDriver webDriver;
         private readonly IMapper _mapper;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly PersonalToolsDbContext _personalToolsDbContext;
@@ -24,6 +28,7 @@ namespace WLVSTools.Web.Controllers
             UserManager<ApplicationUser> userManager,
             PersonalToolsDbContext personalToolsDbContext) : base(userManager)
         {
+            webDriver = new ChromeDriver();
             _mapper = mapper;
             _userManager = userManager;
             _personalToolsDbContext = personalToolsDbContext;
@@ -81,11 +86,29 @@ namespace WLVSTools.Web.Controllers
 
         public IActionResult Lotto()
         {
-            var html = @"https://www.pcso.gov.ph/SearchLottoResult.aspx";
+            return loadLotto(new LottoViewModel());
+        }
 
-            HtmlWeb web = new HtmlWeb();
+        [HttpPost]
+        public IActionResult Lotto(LottoViewModel viewModel)
+        {
+            viewModel.HasFilter = true;
+            return loadLotto(viewModel);
+        }
 
-            var htmlDoc = web.Load(html);
+        private IActionResult loadLotto(LottoViewModel viewModel)
+        {
+            getPCSOLottoResultHtmlString(viewModel);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(webDriver.PageSource);
+
+            while (!htmlDoc.DocumentNode.HasChildNodes)
+            {
+                htmlDoc.LoadHtml(webDriver.PageSource);
+            }
+
+            disposeWebDriver();
 
             var node = htmlDoc.DocumentNode.SelectSingleNode("//*[@id=\"cphContainer_cpContent_GridView1\"]");
 
@@ -93,22 +116,77 @@ namespace WLVSTools.Web.Controllers
 
             node.Attributes.Add("class", "table table-striped");
 
-            return View(new HtmlString(node.OuterHtml));
+            viewModel.HtmlStringOutput = new HtmlString(node.OuterHtml);
+
+            return View("Lotto", viewModel);
+        }
+
+        private void getPCSOLottoResultHtmlString(LottoViewModel viewModel)
+        {
+            var wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            webDriver.Navigate().GoToUrl(@"https://www.pcso.gov.ph/SearchLottoResult.aspx");
+
+            var html = webDriver.FindElement(By.TagName("html"), 5);
+
+            var elementSelectGame = webDriver.FindElement(By.Id("cphContainer_cpContent_ddlSelectGame"), 5);
+            var ddlSelectGame = new SelectElement(elementSelectGame);
+            ddlSelectGame.SelectByValue(viewModel.LottoGame);
+
+            if (viewModel.HasFilter)
+            {
+                //Start Date
+                var elementStartMonth = webDriver.FindElement(By.Id("cphContainer_cpContent_ddlStartMonth"), 5);
+                var ddlStartMonth = new SelectElement(elementStartMonth);
+                ddlStartMonth.SelectByValue(viewModel.StartMonth);
+
+                var elementStartDate = webDriver.FindElement(By.Id("cphContainer_cpContent_ddlStartDate"), 5);
+                var ddlStartDate = new SelectElement(elementStartDate);
+                ddlStartDate.SelectByValue(viewModel.StartDate);
+
+                var elementStartYear = webDriver.FindElement(By.Id("cphContainer_cpContent_ddlStartYear"), 5);
+                var ddlStartYear = new SelectElement(elementStartYear);
+                ddlStartYear.SelectByValue(viewModel.StartYear);
+
+                //End Date
+                var elementEndMonth = webDriver.FindElement(By.Id("cphContainer_cpContent_ddlEndMonth"), 5);
+                var ddlEndMonth = new SelectElement(elementEndMonth);
+                ddlEndMonth.SelectByValue(viewModel.EndMonth);
+
+                var elementEndDate = webDriver.FindElement(By.Id("cphContainer_cpContent_ddlEndDay"), 5);
+                var ddlEndDate = new SelectElement(elementEndDate);
+                ddlEndDate.SelectByValue(viewModel.EndDate);
+
+                var elementEndYear = webDriver.FindElement(By.Id("cphContainer_cpContent_ddlEndYear"), 5);
+                var ddlEndYear = new SelectElement(elementEndYear);
+                ddlEndYear.SelectByValue(viewModel.EndYear);
+            }
+
+            var btnSearchLotto = webDriver.FindElement(By.Id("cphContainer_cpContent_btnSearch"), 5);
+            btnSearchLotto.Click();
+        }
+
+        private void disposeWebDriver()
+        {
+            webDriver.Quit();
+            webDriver.Dispose();
         }
 
         private void cleanNode(HtmlNode node)
         {
-            if (node.HasChildNodes)
+            if (node != null)
             {
-                foreach (var childNode in node.ChildNodes)
+                if (node.HasChildNodes)
                 {
-                    cleanNode(childNode);
+                    foreach (var childNode in node.ChildNodes)
+                    {
+                        cleanNode(childNode);
+                        node.Attributes.Remove();
+                    }
+                }
+                else
+                {
                     node.Attributes.Remove();
                 }
-            }
-            else
-            {
-                node.Attributes.Remove();
             }
         }
     }
---
+++ WLVSTools/WLVSTools.Web/ViewModels/PersonalToolbox/LottoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Html;
+
+namespace WLVSTools.Web.ViewModels.PersonalToolbox
+{
+    public class LottoViewModel
+    {
+        public LottoViewModel()
+        {
+            LottoGame = "0";
+        }
+
+        public bool HasFilter { get; set; }
+
+        public string? LottoGame { get; set; }
+
+        public HtmlString? HtmlStringOutput { get; set; }
+
+        public string? StartMonth { get; set; }
+        public string? StartDate { get; set; }
+        public string? StartYear { get; set;}
+
+        public string? EndMonth { get; set; }
+        public string? EndDate { get; set; }
+        public string? EndYear { get; set; }
+    }
+}
