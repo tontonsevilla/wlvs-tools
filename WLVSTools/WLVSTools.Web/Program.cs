ebed634455665a7a639794647eb6b9d9e0fdf3a5
Winston Sevilla <wsevilla@blastasia.com>
2024-06-26 17:18:04 +0800

    implement mvc settings

--- WLVSTools/WLVSTools.Web/Program.cs
+++ WLVSTools/WLVSTools.Web/Program.cs
@@ -1,6 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Services to the Container
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Configure the HTTP Request Pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+
+    //The default HSTS value is 30 days
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();   
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=index}/{id?}");
 
 app.Run();
