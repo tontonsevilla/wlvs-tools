4e8ba6f46cfe9f6741157d2db0ad3df9411787f7
Winston Sevilla <wsevilla@blastasia.com>
2024-06-27 18:05:33 +0800

    TimeKeeping and other stuff.

---
+++ WLVSTools/WLVSTools.Web/Controllers/BlastAsiaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using WLVSTools.Web.Models.BlastAsia;
+
+namespace WLVSTools.Web.Controllers
+{
+    public class BlastAsiaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TimeIn() 
+        {
+            return View(new TimeKeeping(TimeKeepingType.In));
+        }
+
+        [HttpPost]
+        public IActionResult TimeIn(TimeKeeping model)
+        {
+            return View(model);
+        }
+
+        public IActionResult TimeOut()
+        {
+            return View(new TimeKeeping(TimeKeepingType.Out));
+        }
+
+        [HttpPost]
+        public IActionResult TimeOut(TimeKeeping model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ WLVSTools/WLVSTools.Web/Models/BlastAsia/TimeKeeping.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WLVSTools.Web.Models.BlastAsia
+{
+    public class TimeKeeping
+    {
+        public TimeKeeping()
+        {
+            Body = string.Empty;
+        }
+        public TimeKeeping(TimeKeepingType timeKeepingType)
+        {
+            Body = string.Empty;
+            Type = timeKeepingType;
+
+            switch (timeKeepingType)
+            {
+                case TimeKeepingType.In:
+                    Body = "IM IN";
+                    break;
+                case TimeKeepingType.Out:
+                    Body = "IM OUT";
+                    break;
+                case TimeKeepingType.Unset:
+                default:
+                    break;
+            }
+        }
+
+        public TimeKeepingType Type { get; set; }
+
+        [Display(Name = "Email Recipient")]
+        public string Recipient { get; set; } = "attendance@blastasia.com";
+
+        [Display(Name = "Email CC Recipient")]
+        public string CCRecipient { get; set; } = "rfernandez@blastasia.com";
+
+        [Display(Name = "Email Subject")]
+        public string Subject { get; set; } = $"WFH {DateTime.Now.ToString("MM/dd/yyyy")}";
+
+        [Display(Name = "Email Body")]
+        public string Body { get; set; }
+    }
+
+    public enum TimeKeepingType
+    {
+        Unset,
+        In,
+        Out
+    }
+}
