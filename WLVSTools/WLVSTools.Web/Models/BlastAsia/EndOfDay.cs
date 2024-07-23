1c4f2735acf99c42b8e338fd58245b8422addc32
Winston Sevilla <wsevilla@blastasia.com>
2024-07-23 19:29:35 +0800

    update

--- WLVSTools/WLVSTools.Web/Models/BlastAsia/EndOfDay.cs
+++ WLVSTools/WLVSTools.Web/Models/BlastAsia/EndOfDay.cs
@@ -5,18 +5,18 @@ namespace WLVSTools.Web.Models.BlastAsia
     public class EndOfDay
     {
         [Display(Name = "Steer Account"), Required]
-        public string Email { get; set; } = "";
+        public string? Email { get; set; } = "";
 
         [Display(Name = "Steer Password"), Required]
-        public string Password { get; set; } = "";
+        public string? Password { get; set; } = "";
 
         [Display(Name = "EOD Subject"), Required]
-        public string Subject { get; set; } = $"EOD REPORT FOR {DateTime.Now.ToString("MMM dd, yyyy").ToUpper()}";
+        public string? Subject { get; set; }
 
         [Display(Name = "EOD Date"), Required]
         public DateTime? EODDate { get; set; } = DateTime.Now;
 
         [Display(Name = "EOD Account"), Required]
-        public string EODAccount { get; set; } = "AIFS";
+        public string? EODAccount { get; set; } = "AIFS";
     }
 }
