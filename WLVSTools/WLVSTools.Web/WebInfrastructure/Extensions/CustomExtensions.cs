464de6e2cdf44d8ec91abffc556c456a50408852
Winston Sevilla <wsevilla@blastasia.com>
2024-07-24 13:34:40 +0800

    update

--- WLVSTools/WLVSTools.Web/WebInfrastructure/Extensions/CustomExtensions.cs
+++ WLVSTools/WLVSTools.Web/WebInfrastructure/Extensions/CustomExtensions.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System.Text.Encodings.Web;
+using System.Web;
 
 namespace WLVSTools.Web.WebInfrastructure.Extensions
 {
@@ -15,6 +20,30 @@ namespace WLVSTools.Web.WebInfrastructure.Extensions
         public static void AddErrorMessage(this ModelStateDictionary dictionary, string errorMessage)
         {
             dictionary.AddModelError(string.Empty, errorMessage);
+        }
+
+        public static IHtmlContent CustomValidationSummary(this IHtmlHelper htmlHelper, bool excludePropertyErrors = true)
+        {
+            if (htmlHelper == null) throw new ArgumentNullException("htmlHelper");
+
+            IHtmlContent validationSummary = null;
+
+            if (!htmlHelper.ViewData.ModelState.IsValid && htmlHelper.ViewData.ModelState.ContainsKey(string.Empty))
+            {
+                var htmlAttributes = new { @class = "alert alert-danger" };
+                validationSummary = htmlHelper.ValidationSummary(excludePropertyErrors, "", htmlAttributes);
+            }
+
+            return validationSummary;
+        }
+
+        public static string CustomRaw(this IHtmlHelper htmlHelper, IHtmlContent content)
+        {
+            using (var writer = new System.IO.StringWriter())
+            {
+                content.WriteTo(writer, HtmlEncoder.Default);
+                return HttpUtility.HtmlDecode(writer.ToString());
+            }
         }
     }
 }
