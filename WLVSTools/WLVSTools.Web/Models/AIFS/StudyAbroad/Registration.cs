776391477c1d363cf2f80132ba93c59d83ec0def
Winston Sevilla <wsevilla@blastasia.com>
2024-08-09 08:30:48 +0800

    update

--- WLVSTools/WLVSTools.Web/Models/AIFS/StudyAbroad/Registration.cs
+++ WLVSTools/WLVSTools.Web/Models/AIFS/StudyAbroad/Registration.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 using WLVSTools.Web.Models.DeveloperTools;
 
 namespace WLVSTools.Web.Models.AIFS.StudyAbroad
@@ -12,7 +13,20 @@ namespace WLVSTools.Web.Models.AIFS.StudyAbroad
         public string? State { get; set; }
 
         [Required]
-        public string? Url { get; set; } = "http://localhost/College/Account/Register";
+        public string? Url { get; set; }
+
+        public List<SelectListItem> UrlList
+        {
+            get
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem("Please select", ""),
+                    new SelectListItem("http://localhost/College/Account/Register", "http://localhost/College/Account/Register"),
+                    new SelectListItem("https://securestg.aifsabroad.com/College/Account/Register", "https://securestg.aifsabroad.com/College/Account/Register")
+                };
+            }
+        }
 
         public Personalnfo? Personalnfo { get; set; }
     }
